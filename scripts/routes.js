d6deb59b6a22fab5a2e19d311d322d687b977131
Christoph Ruggenthaler <christoph.ruggenthaler@gmail.com>
2015-08-04 14:05:39 +0200

    added twitter unwetter show case

--- scripts/routes.js
+++ scripts/routes.js
@@ -31,9 +31,12 @@ angular.module('ubicity').config([ '$stateProvider', '$urlRouterProvider', funct
     }).state("demo.dashboard", {
 	url : "/dashboard",
 	templateUrl : 'views/demo.dashboard.html'
-    }).state("demo.twitter", {
-	url : "/twitter",
-	templateUrl : 'views/demo.twitter.html'
+    }).state("demo.twitter-geo", {
+	url : "/twitter-geo",
+	templateUrl : 'views/demo.twitter-geo.html'
+    }).state("demo.twitter-topic", {
+	url : "/twitter-topic",
+	templateUrl : 'views/demo.twitter-topic.html'
     }).state("demo.rss", {
 	url : "/rss",
 	templateUrl : 'views/demo.rss.html'
